4257e62d5a7339c9cb212a0c815d6e2a1de774a1
baby9 <mingtangwan@gmail.com>
2023-06-29 17:10:44 +0800

    update files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+cat sb-example.json > sb-config.json
+
 if [ ! -f warp.yml ]; then
     ./WarpRegister > warp.yml
-    mv sb-example.json sb-config.json
     sed -i "s#$(cat sb-config.json | grep 'local_address' | awk -F ',' '{print $2}' | awk -F '"' '{print $2}' | awk -F '/' '{print $1}')#$(cat warp.yml | grep 'v6:' | cut -d " " -f 2)#" sb-config.json
     sed -i "s#$(cat sb-config.json | grep 'private_key' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$(cat warp.yml | grep 'private_key:' | cut -d " " -f 2)#" sb-config.json
     sed -i "s#$(cat sb-config.json | grep 'peer_public_key' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$(cat warp.yml | grep 'public_key:' | cut -d " " -f 2)#" sb-config.json
@@ -13,8 +14,8 @@ if [ ! -z $SOCKS_USER ] && [ ! -z $SOCKS_PASSWORD ]; then
     sed -i "s#$(cat sb-config.json | grep 'username' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$SOCKS_USER#" sb-config.json
     sed -i "s#$(cat sb-config.json | grep 'password' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$SOCKS_PASSWORD#" sb-config.json
 else
-    sed -i "s#$(cat sb-config.json | grep 'username' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')##" sb-config.json
-    sed -i "s#$(cat sb-config.json | grep 'password' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')##" sb-config.json
+    sed -i "$(awk '/username/{print NR}' sb-config.json)d" sb-config.json
+    sed -i "$(awk '/password/{print NR}' sb-config.json)d" sb-config.json
 fi
 
 socat TCP-LISTEN:40000,fork TCP:localhost:40001 &
