6c1e4b3f615ed5d9a6cacd893669d45504026ff5
baby9 <mingtangwan@gmail.com>
2023-06-29 18:33:57 +0800

    update files

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,19 @@ cat sb-example.json > sb-config.json
 
 if [ ! -f warp.yml ]; then
     ./WarpRegister > warp.yml
-    sed -i "s#$(cat sb-config.json | grep 'local_address' | awk -F ',' '{print $2}' | awk -F '"' '{print $2}' | awk -F '/' '{print $1}')#$(cat warp.yml | grep 'v6:' | cut -d " " -f 2)#" sb-config.json
-    sed -i "s#$(cat sb-config.json | grep 'private_key' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$(cat warp.yml | grep 'private_key:' | cut -d " " -f 2)#" sb-config.json
-    sed -i "s#$(cat sb-config.json | grep 'peer_public_key' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$(cat warp.yml | grep 'public_key:' | cut -d " " -f 2)#" sb-config.json
-    sed -i "s#$(cat sb-config.json | grep 'reserved' | awk -F '[' '{print $2}' | awk -F ']' '{print $1}')#$(cat warp.yml | grep 'reserved:' | cut -d "[" -f 2 | cut -d "]" -f 1)#" sb-config.json
 fi
+
+sed -i "s#$(cat sb-config.json | grep 'local_address' | awk -F ',' '{print $2}' | awk -F '"' '{print $2}' | awk -F '/' '{print $1}')#$(cat warp.yml | grep 'v6:' | cut -d " " -f 2)#" sb-config.json
+sed -i "s#$(cat sb-config.json | grep 'private_key' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$(cat warp.yml | grep 'private_key:' | cut -d " " -f 2)#" sb-config.json
+sed -i "s#$(cat sb-config.json | grep 'peer_public_key' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$(cat warp.yml | grep 'public_key:' | cut -d " " -f 2)#" sb-config.json
+sed -i "s#$(cat sb-config.json | grep 'reserved' | awk -F '[' '{print $2}' | awk -F ']' '{print $1}')#$(cat warp.yml | grep 'reserved:' | cut -d "[" -f 2 | cut -d "]" -f 1)#" sb-config.json
 
 if [ ! -z $SOCKS_USER ] && [ ! -z $SOCKS_PASSWORD ]; then
     sed -i "s#$(cat sb-config.json | grep 'username' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$SOCKS_USER#" sb-config.json
     sed -i "s#$(cat sb-config.json | grep 'password' | awk -F ':' '{print $2}' | awk -F '"' '{print $2}')#$SOCKS_PASSWORD#" sb-config.json
 else
+    sed -i "$(awk '/username/{print NR-1}' sb-config.json)d" sb-config.json
+    sed -i "$(awk '/password/{print NR+1}' sb-config.json)d" sb-config.json
     sed -i "$(awk '/username/{print NR}' sb-config.json)d" sb-config.json
     sed -i "$(awk '/password/{print NR}' sb-config.json)d" sb-config.json
 fi
