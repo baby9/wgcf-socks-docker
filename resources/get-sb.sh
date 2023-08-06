87cbecfa476ff420af5efa73dd9f07e0eed9c639
baby9 <67196418+baby9@users.noreply.github.com>
2023-08-06 18:06:02 +0800

    Initial commit

---
+++ resources/get-sb.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+case $TARGETARCH in
+"arm")
+    Sing_arch="sing-box-1.3.0-linux-armv7"
+    ;;
+"arm64")
+    Sing_arch="sing-box-1.3.0-linux-arm64"
+    ;;
+"amd64")
+    Sing_arch="sing-box-1.3.0-linux-amd64"
+    ;;
+*)
+    exit 1
+    ;;
+esac
+
+wget "https://github.com/SagerNet/sing-box/releases/download/v1.3.0/${Sing_arch}.tar.gz"
+tar xvf "${Sing_arch}.tar.gz"
+mv "${Sing_arch}/sing-box" .
+rm -rf sing-box-*
