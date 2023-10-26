6df7abcd98211cf140b3719309a42afe03c0e503
baby9 <67196418+baby9@users.noreply.github.com>
2023-10-26 17:21:50 +0800

    Add s390x support

--- res/get-sb.sh
+++ res/get-sb.sh
@@ -2,13 +2,16 @@
 
 case $TARGETARCH in
 "arm")
-    Sing_arch="sing-box-1.3.0-linux-armv7"
+    Sing_arch="sing-box-1.5.0-linux-armv7"
     ;;
 "arm64")
-    Sing_arch="sing-box-1.3.0-linux-arm64"
+    Sing_arch="sing-box-1.5.0-linux-arm64"
     ;;
 "amd64")
-    Sing_arch="sing-box-1.3.0-linux-amd64"
+    Sing_arch="sing-box-1.5.0-linux-amd64"
+    ;;
+"s390x")
+    Sing_arch="sing-box-1.5.0-linux-s390x"
     ;;
 *)
     echo "error: architecture not supported"
@@ -16,7 +19,7 @@ case $TARGETARCH in
     ;;
 esac
 
-wget "https://github.com/SagerNet/sing-box/releases/download/v1.3.0/${Sing_arch}.tar.gz"
+wget "https://github.com/SagerNet/sing-box/releases/download/v1.5.0/${Sing_arch}.tar.gz"
 tar xvf "${Sing_arch}.tar.gz"
 mv "${Sing_arch}/sing-box" .
 rm -rf sing-box-*
