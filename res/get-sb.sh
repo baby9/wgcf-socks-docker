8ad2721c63b202c72ea7cb91cf3cbd174797f728
baby9 <67196418+baby9@users.noreply.github.com>
2024-05-12 20:04:25 +0800

    Add 386 support

--- res/get-sb.sh
+++ res/get-sb.sh
@@ -2,16 +2,19 @@
 
 case $TARGETARCH in
 "arm")
-    Sing_arch="sing-box-1.5.0-linux-armv7"
+    Sing_arch="sing-box-1.7.0-linux-armv7"
     ;;
 "arm64")
-    Sing_arch="sing-box-1.5.0-linux-arm64"
+    Sing_arch="sing-box-1.7.0-linux-arm64"
+    ;;
+"386")
+    Sing_arch="sing-box-1.7.0-linux-386"
     ;;
 "amd64")
-    Sing_arch="sing-box-1.5.0-linux-amd64"
+    Sing_arch="sing-box-1.7.0-linux-amd64"
     ;;
 "s390x")
-    Sing_arch="sing-box-1.5.0-linux-s390x"
+    Sing_arch="sing-box-1.7.0-linux-s390x"
     ;;
 *)
     echo "error: architecture not supported"
@@ -19,7 +22,7 @@ case $TARGETARCH in
     ;;
 esac
 
-wget "https://github.com/SagerNet/sing-box/releases/download/v1.5.0/${Sing_arch}.tar.gz"
+wget "https://github.com/SagerNet/sing-box/releases/download/v1.7.0/${Sing_arch}.tar.gz"
 tar xvf "${Sing_arch}.tar.gz"
 mv "${Sing_arch}/sing-box" .
 rm -rf sing-box-*
